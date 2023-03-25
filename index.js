2bcfea709be66cfadc5f650744213df1d75e7550
Vijeth16 <vijeth.5448@gmail.com>
2023-03-25 15:39:56 +0530

    update print

--- index.js
+++ index.js
@@ -2,21 +2,21 @@
 //var read = require('readline-sync')
 //const a = undefined
 var user = prompt("Enter your name-")
-document.write("Welcome, "+user+" for Tech quiz")
+ window.print("Welcome, "+user+" for Tech quiz")
 var score =0
-//document.write("Press ENTER to continue")
+// window.print("Press ENTER to continue")
 function userques(Question,Answer){     //function
   var usranswer=prompt(Question);  // it will display questions passed in arr
-  //document.write(usranswer)
+  // window.print(usranswer)
   
   if(usranswer===Answer)
   {
-    document.write("correct")
+     window.print("correct")
     score = score+1
-    document.write(score)
+     window.print(score)
   }
   else{
-    document.write("wrong")
+     window.print("wrong")
   
   }
   
@@ -34,5 +34,5 @@ for(i=0;i<arr.length;i++){
   var arr1 = arr[i];
   //userques(arr.question,arr.answer)
   userques(arr1.question,arr1.answer); //function call
-  document.write(userques);
+   window.print(userques);
 }
